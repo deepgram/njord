5d2c2f56555ce73ea5864c661613c325cb1a934e
Adam Sypniewski <ajsyp@syptech.net>
2025-10-21 14:36:08 -0400

    Improved flake.nix

--- flake.nix
+++ flake.nix
@@ -2,12 +2,21 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+
+        # Define Rust toolchain with musl target
+        rustToolchain = pkgs.rust-bin.stable.latest.default.override {
+          targets = [ "x86_64-unknown-linux-musl" ];
+        };
         
         njord = pkgs.rustPlatform.buildRustPackage {
           pname = "njord";
@@ -25,6 +34,19 @@
         packages.default = njord;
         apps.default = flake-utils.lib.mkApp {
           drv = njord;
+        };
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            rustToolchain
+            pkgs.pkgsStatic.stdenv.cc
+          ];
+
+          CARGO_BUILD_TARGET = "x86_64-unknown-linux-musl";
+          CARGO_TARGET_X86_64_UNKNOWN_LINUX_MUSL_LINKER = "${pkgs.pkgsStatic.stdenv.cc}/bin/${pkgs.pkgsStatic.stdenv.cc.targetPrefix}cc";
+
+          shellHook = ''
+            export CC_x86_64_unknown_linux_musl="${pkgs.pkgsStatic.stdenv.cc}/bin/${pkgs.pkgsStatic.stdenv.cc.targetPrefix}cc"
+          '';
         };
       });
 }
